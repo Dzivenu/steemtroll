6ed137d24edcb59079671e1b156dc2d9bdfc82e6
antonchanning <antonchanning@gmail.com>
2017-08-06 14:40:27 +0100

    // Tries to load custom content, if not default content. Then checks querystring to see which should be loaded.

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
     // SteemTroll
     // Don't try to control or the troll will con you...
     error_reporting(E_ALL);
@@ -12,4 +12,19 @@
     // echo BASE_URL;
     // echo BASE_PATH;
     require_once(BASE_PATH.'/config/config.php');
+ 
+    // Check for custom content
+	if(file_exists(BASE_PATH.'/content/local-content.php')) {
+		require_once(BASE_PATH.'/content/local-content.php');
+	} else {
+		require_once(BASE_PATH.'/content/default-content.php');
+	}
+	
+	// Check if some content was selected, or use the default
+ 	if(isset($_GET['p'])) {
+ 		$page->content[] = $contentArray[$_GET['p']];
+	} else {
+		$page->content[] = $contentArray[$defaultContent];
+	}
+    
     $page->renderPage();
